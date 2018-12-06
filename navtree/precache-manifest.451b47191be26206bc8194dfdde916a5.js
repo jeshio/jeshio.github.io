f384fdaf9224d0c626fa1e48844f683d91f98bcd
Georgiy Ivanov <jeshio@yandex.ru>
2018-12-06 20:35:34 +0500

    add navtree build for preview

---
+++ navtree/precache-manifest.451b47191be26206bc8194dfdde916a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dfa1ad5b076f157f86a2",
+    "url": "/static/js/main.dfa1ad5b.chunk.js"
+  },
+  {
+    "revision": "7a39d641c763b96b70c5",
+    "url": "/static/js/1.7a39d641.chunk.js"
+  },
+  {
+    "revision": "dfa1ad5b076f157f86a2",
+    "url": "/static/css/main.cac0f755.chunk.css"
+  },
+  {
+    "revision": "7a39d641c763b96b70c5",
+    "url": "/static/css/1.73b57d04.chunk.css"
+  },
+  {
+    "revision": "b8193782b78f862563212945d8e3bc50",
+    "url": "/index.html"
+  }
+];
