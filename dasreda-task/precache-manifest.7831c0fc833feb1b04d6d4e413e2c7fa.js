18a2eebdfb7cdfd0348983bace8cdfd96af26d5f
jeshio <jeshio@yandex.ru>
2019-05-17 16:31:30 +0500

    fix dasreda-task

---
+++ dasreda-task/precache-manifest.7831c0fc833feb1b04d6d4e413e2c7fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6301537b8ac6c22e6b42abb066e1719c",
+    "url": "/dasreda-task/index.html"
+  },
+  {
+    "revision": "37a7f8cb162ee13930c6",
+    "url": "/dasreda-task/static/css/2.79fd74fd.chunk.css"
+  },
+  {
+    "revision": "37a7f8cb162ee13930c6",
+    "url": "/dasreda-task/static/js/2.28fc9810.chunk.js"
+  },
+  {
+    "revision": "02ca24a2f65d262034a0",
+    "url": "/dasreda-task/static/js/main.efe5b684.chunk.js"
+  },
+  {
+    "revision": "39598923c1ba2106dcfd",
+    "url": "/dasreda-task/static/js/runtime~main.8dc283a6.js"
+  }
+]);
--- dasreda-task/service-worker.js
+++ dasreda-task/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.57fde324690aa425b667fa86d1bc8043.js"
+  "/dasreda-task/precache-manifest.7831c0fc833feb1b04d6d4e413e2c7fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dasreda-task/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
